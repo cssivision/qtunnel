3ebab13c5aca20f1fe0c6d70d939e66a23ac6d8a
cssivision <cssivision@gmail.com>
2021-08-13 22:37:12 +0800

    init server

--- src/bin/qtunnel-client.rs
+++ src/bin/qtunnel-client.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::io::{self, Write};
+
+use qtunnel::ALPN_QUIC_HTTP;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    env_logger::init();
+
+    let mut endpoint = quinn::Endpoint::builder();
+    let mut client_config = quinn::ClientConfigBuilder::default();
+    client_config.protocols(ALPN_QUIC_HTTP);
+    client_config
+        .add_certificate_authority(
+            quinn::Certificate::from_pem(&fs::read("tls_config/rootCA.crt")?).unwrap(),
+        )
+        .unwrap();
+
+    endpoint.default_client_config(client_config.build());
+    let (endpoint, _) = endpoint.bind(&"127.0.0.1:0".parse().unwrap()).unwrap();
+    let new_conn = endpoint
+        .connect(&"127.0.0.1:8091".parse().unwrap(), "mydomain.com")
+        .unwrap()
+        .await
+        .unwrap();
+
+    let quinn::NewConnection {
+        connection: conn, ..
+    } = new_conn;
+    let (mut send, recv) = conn.open_bi().await.unwrap();
+
+    send.write_all(b"helloworld").await.unwrap();
+    send.finish().await.unwrap();
+    let resp = recv.read_to_end(usize::max_value()).await.unwrap();
+    io::stdout().write_all(&resp).unwrap();
+    io::stdout().flush().unwrap();
+    conn.close(0u32.into(), b"done");
+
+    // Give the server a fair chance to receive the close packet
+    endpoint.wait_idle().await;
+
+    Ok(())
 }
--- src/bin/qtunnel-server.rs
+++ src/bin/qtunnel-server.rs
@@ -1,3 +1,63 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::io;
+use std::sync::Arc;
+
+use futures_util::StreamExt;
+use qtunnel::ALPN_QUIC_HTTP;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    env_logger::init();
+
+    let mut transport_config = quinn::TransportConfig::default();
+    transport_config.max_concurrent_uni_streams(0).unwrap();
+    let mut server_config = quinn::ServerConfig::default();
+    server_config.transport = Arc::new(transport_config);
+    let mut server_config = quinn::ServerConfigBuilder::new(server_config);
+    server_config.protocols(ALPN_QUIC_HTTP);
+
+    let key = fs::read("tls_config/mydomain.com.key").unwrap();
+    let key = quinn::PrivateKey::from_pem(&key).unwrap();
+    let cert_chain = fs::read("tls_config/mydomain.com.crt").unwrap();
+    let cert_chain = quinn::CertificateChain::from_pem(&cert_chain).unwrap();
+    server_config.certificate(cert_chain, key).unwrap();
+
+    let mut endpoint = quinn::Endpoint::builder();
+    endpoint.listen(server_config.build());
+
+    let (endpoint, mut incoming) = endpoint.bind(&"127.0.0.1:8091".parse().unwrap()).unwrap();
+    log::debug!("listening on {}", endpoint.local_addr().unwrap());
+
+    while let Some(conn) = incoming.next().await {
+        log::info!("connection incoming");
+        tokio::spawn(handle_connection(conn));
+    }
+    Ok(())
+}
+
+async fn handle_connection(conn: quinn::Connecting) {
+    let quinn::NewConnection { mut bi_streams, .. } = conn.await.unwrap();
+    log::info!("established");
+    // Each stream initiated by the client constitutes a new request.
+    while let Some(stream) = bi_streams.next().await {
+        let stream = match stream {
+            Err(quinn::ConnectionError::ApplicationClosed { .. }) => {
+                log::info!("connection closed");
+                return;
+            }
+            Err(e) => {
+                log::info!("connection err: {:?}", e);
+                return;
+            }
+            Ok(s) => s,
+        };
+        tokio::spawn(handle_request(stream));
+    }
+}
+
+async fn handle_request((mut send, recv): (quinn::SendStream, quinn::RecvStream)) {
+    let req = recv.read_to_end(64 * 1024).await.unwrap();
+    send.write_all(&req).await.unwrap();
+    send.finish().await.unwrap();
+    log::info!("complete");
 }
